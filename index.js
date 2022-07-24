e9abfb1f21a152d2e8dd782c6f15215bc49a3b1b
Dean <deank242@gmail.com>
2022-07-24 14:02:05 -0400

    final updates

--- index.js
+++ index.js
@@ -1,148 +1,408 @@
-// TODO: Include packages needed for this application
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./src/generateMarkdown.js')
-// TODO: Create an array of questions for user input
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+// array of questions for user
 const questions = [
     {
-        type: "input",
-        name: "title",
-        message: "What is the title of your project? (Required)",
-        validate: inputTitle => {
-            if (inputTitle) {
+        type: 'input',
+        name: 'title',
+        message: 'Please provide a project title.  (Required)',
+        validate: nameInput => {
+            if (nameInput) {
                 return true;
             } else {
-                console.log('Please enter a title!')
+                console.log('Please provide a project title!');
                 return false;
             }
         }
     },
-
     {
-        type: "input",
-        name: "description",
-        message: "Provide a description for your project? (Required)",
-        validate: inputDescription => {
-            if (inputDescription) {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username. (Required)',
+        validate: githubInput => {
+            if (githubInput) {
                 return true;
             } else {
-                console.log('Please enter a description!')
+                console.log('Please enter your GitHub username!');
                 return false;
             }
-        } 
+        }
     },
-
     {
-        type: "checkbox",
-        name: "languages",
-        message: "What did you this project with? (Check all that apply)",
+        type: 'input',
+        name: 'repo',
+        message: 'Please enter the name of your repo. (Required)',
+        validate: repoInput => {
+            if (repoInput) {
+                return true;
+            } else {
+                console.log('Please enter the name of your repo!')
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your application. (Required)',
+        validate: descInput => {
+            if (descInput) {
+                return true;
+            } else {
+                console.log('Please enter a description!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide information for using your application. (Required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please provide information for using your application!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'checkbox',
+        name: 'contents',
+        message: 'Any additional sections you would like to include in your README?',
         choices: [
-          "JavaScript",
-          "HTML",
-          "CSS",
-          "ES6",
-          "jQuery",
-          "Bootstrap",
-          "Node",
-        ],
-      },
-
+            {
+                name: 'Deployed Application',
+                checked: false
+            },
+            {
+                name: 'Installation',
+                checked: false
+            },
+            {
+                name: 'Screenshots',
+                checked: true
+            },
+            {
+                name: 'Built With',
+                checked: true
+            },
+            {
+                name: 'License',
+                checked: false
+            },
+            {
+                name: 'Contributing',
+                checked: false
+            },
+            {
+                name: 'Tests',
+                checked: false
+            },
+            {
+                name: 'Questions',
+                checked: true
+            },
+            {
+                name: 'Credits',
+                checked: true
+            },
+        ]
+    },
     {
-        type: "input",
-        name: "usage",
-        message: "How do you use this project? (Required)",
-        validate: inputUsage => {
-            if (inputUsage) {
+        type: 'input',
+        name: 'link',
+        message: 'Please provide a link to your deployed application.',
+        when: ({ contents }) => {
+            if (contents.indexOf('Deployed Application') > -1) {
                 return true;
-            } else {
-                console.log('Please information on how to use the project!')
+            } else { 
                 return false;
             }
-        }
-    },
-
-    {
-        type: "input",
-        name: "contributions",
-        message: "How can people contribute to your project? (Required)",
-        validate: inputContributions => {
-            if (inputContributions) {
-                return true;
-            } else {
-                console.log('You need to provide information explaining how to contribute!')
-                return false;
-            }
-        }
-    },
-
-    {
-        type: "input",
-        name: "testing",
-        message: "how do you test this? (Required)",
-        validate: inputTesting => {
-            if (inputTesting) {
-                return true;
-            } else {
-                console.log('Explain how you test this project!')
-                return false;
-            }
-        }
-    },
-
-    {
-        type: "checkbox",
-        name: "licensing",
-        message: "Choose which license for your project (Required)",
-        choice: ['Apache', 'MIT', 'Mozilla-Public', 'common-development-and-distribution', 'none'],
-        validate: inputLicenses => {
-            if (inputLicenses) {
-                return true;
-            } else {
-                console.log('You have to pick a license for your project!')
-                return false;
-            }
-        }
-    },
-
-    {
-        type: "input",
-        name: "link",
-        message: "Enter the GitHub link to your project. (Required)",
-        validate: (linkInput) => {
-          if (linkInput) {
-            return true;
-          } else {
-            console.log("You need to enter a project GitHub link!");
-            return false;
-          }
         },
-      },
-
-      {
-          type: 'input',
-          name: 'email',
-          message: 'Would you like to include your email'
-      }
+        validate: linkInput => {
+            if (linkInput) {
+                return true;
+            } else {
+                console.log('Please enter a link!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please list any required packages for installation of your application.',
+        when: ({ contents }) => {
+            if (contents.indexOf('Installation') > -1) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: installInput => {
+            if (installInput) {
+                return true;
+            } else {
+                console.log('Please enter installation instructions!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please provide license information.',
+        choices: ['MIT', 'GNU', 'Apache 2.0', 'ISC'],
+        default: 0,
+        when: ({ contents }) => {
+            if (contents.indexOf('License') > -1) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: licenseInput => {
+            if (licenseInput) {
+                return true;
+            } else {
+                console.log('Please provide license information!');
+                return false;
+            }
+        }
+    }, 
+    {
+        type: 'checkbox',
+        name: 'built with',
+        message: 'Please select the technologies that your application was built with.',
+        choices: ['HTML', 'CSS', 'SASS', 'JavaScript', 'Node.js', 'Express.js'],
+        default: 0,
+        when: ({ contents }) => {
+            if (contents.indexOf('Built With') > -1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }, 
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please enter your guidelines for contributing.',
+        when: ({ contents }) => {
+            if (contents.indexOf('Contributing') > -1) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: contributingInput => {
+            if (contributingInput) {
+                return true;
+            } else {
+                console.log('Please enter guidelines for contributing!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please enter test information for your application.',
+        when: ({ contents }) => {
+            if (contents.indexOf('Tests') > -1) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: testsInput => {
+            if (testsInput) {
+                return true;
+            } else {
+                console.log('What packages are required to run tests for your application?');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Please provide an email address for others to reach you with questions.',
+        when: ({ contents }) => {
+            if (contents.indexOf('Questions') > -1) {
+                return true;
+            } else { 
+                return false;
+            }
+        },
+        validate: questionsInput => {
+            if (questionsInput) {
+                return true;
+            } else {
+                console.log('Please provide an email address!');
+                return false;
+            }
+        }
+    }
 ];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-        if (err)
-            throw err;
-        console.log('Success! Information transferred to READMe')
-    });
-};
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions)
+// array of prompts for adding screenshots
+const screenshotQues = [
+    {
+        type: 'input',
+        name: 'screenshotLink',
+        message: 'Please provide a link for your screenshot. (Required)',
+        validate: screenshotLinkInput => {
+            if (screenshotLinkInput) {
+                return true;
+            } else {
+                console.log('Please provide a link for your screenshot!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'screenshotAlt',
+        message: 'Please provide alt text for your screenshot. (Required)',
+        validate: screenshotAltInput => {
+            if (screenshotAltInput) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'screenshotDesc',
+        message: 'Please provide a description of your screenshot. (Optional)'
+    },
+    {
+        type: 'confirm',
+        name: 'confirmAddScreenshot',
+        message: 'Would you like to add another screenshot?',
+        default: false
+    }
+];
+// array of prompts for adding credits
+const creditQues = [
+    {
+        type: 'input',
+        name: 'creditName',
+        message: 'Please give your credit a name. (Required)',
+        validate: creditName => {
+            if (creditName) {
+                return true;
+            } else {
+                console.log('Please enter a name for the credit!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'creditLink',
+        message: 'Please provide a link for the credit.  (Required)',
+        validate: creditLink => {
+            if (creditLink) {
+                return true;
+            } else {
+                console.log('Please enter a name for the credit!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmAddCredit',
+        message: 'Would you like to add another credit?',
+        default: false
+    }
+]
+// recursive function for adding screenshots
+addScreenshots = readmeData => {
     
-    .then(function (userInput) {
-        console.log(userInput)
-        writeToFile("README.md", generateMarkdown(userInput));
+    // initiates screenshot array
+    if (!readmeData.screenshots) {
+        readmeData.screenshots = [];
+    }
+    console.log(`
+==================
+Add New Screenshot
+==================
+    `);
+    return inquirer.prompt(screenshotQues)
+    .then(screenshotData => {
+        // adds the screenshot to the array
+        readmeData.screenshots.push(screenshotData);
+        // will call addScreenshots again based on user input
+        if (screenshotData.confirmAddScreenshot) {
+            return addScreenshots(readmeData);
+        } else {
+            return readmeData;
+        };
     });
 };
-
-init();
-
+// recursive function for adding credits
+addCredits = readmeInfo => {
+    
+    // initiates array for credits
+    if (!readmeInfo.credits) {
+        readmeInfo.credits = [];
+    };
+    console.log(`
+==============
+Add New Credit
+==============
+    `);
+    return inquirer.prompt(creditQues)
+    .then(creditData => {
+        // adds credits to array
+        readmeInfo.credits.push(creditData);
+        // will call addCredits again based on user input
+        if (creditData.confirmAddCredit) {
+            return addCredits(readmeInfo);
+        } else {
+            return readmeInfo;
+        }
+    });
+};
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(`./dist/${fileName}`, data, err => {
+        if (err) {
+            throw err
+        };
+        console.log('README created!')
+    });
+};
+// function to initialize program
+function init() {
+    return inquirer.prompt(questions);
+};
+// function call to initialize program
+init()
+    .then(userResponse => { 
+        // calls function to add screenshots based on user selection
+        if (userResponse.contents.indexOf('Screenshots') > -1) {
+            return addScreenshots(userResponse);
+        } else {
+            return userResponse;
+        }
+    })
+    .then(response => {
+        // calls function to add credits based on user selection
+        if (response.contents.indexOf('Credits') > -1) {
+            return addCredits(response);
+        } else {
+            return response;
+        }
+    })
+    .then(answers => generateMarkdown(answers))
+    .then(generatedReadme => writeToFile('README.md', generatedReadme))
+    .catch(err => {
+        console.log(err);
+    });
